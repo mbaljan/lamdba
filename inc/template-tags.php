6c6e6de15d4f863d7a230308de54aa39b529a7e4
Martin Baljan <martin.baljan@bayard-consulting.com>
2015-08-28 14:12:36 +0200

    minor styling changes for links

--- inc/template-tags.php
+++ inc/template-tags.php
@@ -87,7 +87,7 @@ function parallax_one_posted_on() {
 	);
 
 	$byline = sprintf(
-		_x( 'by %s', 'post author', 'parallax-one' ),
+		_x( 'von %s', 'post author', 'parallax-one' ),
 		'<span class="author vcard"><a class="url fn n" href="' . esc_url( get_author_posts_url( get_the_author_meta( 'ID' ) ) ) . '">' . esc_html( get_the_author() ) . '</a></span>'
 	);
 
