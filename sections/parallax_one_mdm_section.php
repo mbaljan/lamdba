fea28637d6c1bf55e7ef27e75a6a407b19f63059
Martin Baljan <martin.baljan@bayard-consulting.com>
2015-10-07 18:55:09 +0200

    added new class for teaser images added theme description & new screenshot css/php/html code clean up

--- sections/parallax_one_mdm_section.php
+++ sections/parallax_one_mdm_section.php
@@ -7,7 +7,7 @@
 
 //TODO: integrate in backend
 
-	get_header(); 
+	get_header();
 ?>
     <section class="services" id="services">
         <div class="section-overlay-layer">
@@ -17,56 +17,58 @@
                 <div class="section-header">
 
 								<h2 class="dark-text">MDM Seminare</h2><div class="colored-line"></div>
-					
+
 								<h2 class="dark-text paralax_one_only_customizer"></h2><div class="colored-line paralax_one_only_customizer"></div>
 
-								<div class="sub-heading">Test</div>
-						
+								<div class="sub-heading">Profitieren Sie von unserer Expertise und lassen Sie sich von unseren Experten trainieren.</div>
+
+                                                                                                <span class="teaser-seminare"></span>
+
 								<div class="sub-heading paralax_one_only_customizer"></div>
                 </div>
 
                 <div id="our_services_wrap" class="services-wrap services-wrap parallax_one_grid parallax_one_grid_sqZq7wjtIE">
-								
+
 				    <div class="parallax_one_grid_col_3 parallax_one_grid_column_1 parallax_one_grid_first mdmContainer">
-                        
+
                         <div class="single-service border-bottom-hover mdm">
 
-                        <h3 class="colored-text mdm">MDM Seminar</h3>
-										
+                        <h3 class="colored-text mdm">Seminare</h3>
+
                         <p>Lorem ipsum dolor sit amet, consectetuer adipiscing elit. Aenean commodo ligula eget dolor. Aenean massa. Cum sociis natoque penatibus et magnis dis parturient montes, nascetur ridiculus mus. Donec                            quam felis, ultricies nec, pellentesque eu, pretium quis, sem. Nulla consequat massa quis enim. Donec pede justo, fringilla vel, aliquet nec, vulputate eget, arcu.</p>
-											
+
                         </div>
-                        
+
                     </div>
-                    
+
                      <div class="parallax_one_grid_col_3 parallax_one_grid_column_2 mdmContainer">
-                        
+
                         <div class="single-service border-bottom-hover mdm">
 
                         <h3 class="colored-text mdm">MDM Seminar</h3>
-										
+
                         <p>Lorem ipsum dolor sit amet, consectetuer adipiscing elit. Aenean commodo ligula eget dolor. Aenean massa. Cum sociis natoque penatibus et magnis dis parturient montes, nascetur ridiculus mus. Donec                            quam felis, ultricies nec, pellentesque eu, pretium quis, sem. Nulla consequat massa quis enim. Donec pede justo, fringilla vel, aliquet nec, vulputate eget, arcu.</p>
-											
+
                         </div>
-                        
+
                     </div>
-                    
+
                       <div class="parallax_one_grid_col_3 parallax_one_grid_column_3 parallax_one_grid_last mdmContainer">
-                        
+
                         <div class="single-service border-bottom-hover mdm">
 
                         <h3 class="colored-text mdm">MDM Seminar</h3>
-										
+
                         <p>Lorem ipsum dolor sit amet, consectetuer adipiscing elit. Aenean commodo ligula eget dolor. Aenean massa. Cum sociis natoque penatibus et magnis dis parturient montes, nascetur ridiculus mus. Donec                            quam felis, ultricies nec, pellentesque eu, pretium quis, sem. Nulla consequat massa quis enim. Donec pede justo, fringilla vel, aliquet nec, vulputate eget, arcu.</p>
-											
+
                         </div>
-                        
+
                     </div>
-		
+
                 </div>
 
             </div>
-            
+
         </div>
-        
+
     </section>
